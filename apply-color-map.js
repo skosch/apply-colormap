f5e1c2fc561861fa1ca355fab91ccb0e1b754ff7
Sebastian Kosch <sebastian@aldusleaf.org>
2019-12-01 20:43:46 -0800

    Add alpha channel to apply-colormap

--- apply-color-map.js
+++ apply-color-map.js
@@ -8,8 +8,8 @@ var colormap = require("colormap")
 var cwise = require("cwise")
 
 var doColoring = cwise({
-  args: ["array", "array", "array", "array", "scalar", "scalar", "scalar"],
-  body: function(out_r, out_g, out_b, inp, cmap, lo, hi) {
+  args: ["array", "array", "array", "array", "array", "scalar", "scalar", "scalar", "scalar"],
+  body: function(out_r, out_g, out_b, out_a, inp, cmap, lo, hi, alpha) {
     var idx = ((inp - lo) * (cmap.length-1) / (hi - lo)) | 0
     if(idx < 0) {
       idx = 0
@@ -20,6 +20,7 @@ var doColoring = cwise({
     out_r = cmap[idx][0]
     out_g = cmap[idx][1]
     out_b = cmap[idx][2]
+    out_a = alpha;
   }
 })
 
@@ -27,6 +28,7 @@ function applyColorMap(array, options) {
   options = options || {}
   var lo = -Infinity
   var hi = Infinity
+  var alpha = 255;
   if("min" in options) {
     lo = +options.min
   } else {
@@ -37,25 +39,28 @@ function applyColorMap(array, options) {
   } else {
     hi = ops.sup(array)
   }
+  if("alpha" in options) {
+    alpha = +options.alpha;
+  }
   var cmap = colormap({
     colormap: options.colormap || "jet",
     nshades: 256,
     format: "rgb"
   })
-  var out = options.outBuffer || new Uint8Array(array.size * 3)
+  var s = 4;
+  var out = options.outBuffer || new Uint8Array(array.size * s)
   var out_shape = new Array(array.dimension + 1)
   var out_stride = new Array(array.dimension)
-  var s = 3
-  for(var i=array.dimension-1; i>=0; --i) {
+  for (var i=array.dimension-1; i>=0; --i) {
     out_stride[i] = s
     out_shape[i] = array.shape[i]
     s *= array.shape[i]
   }
-  out_shape[array.dimension] = 3
-  var out_colors = new Array(3)
-  for(var i=0; i<3; ++i) {
-    out_colors[i] = ndarray(out, array.shape, out_stride, i)
+  out_shape[array.dimension] = 4
+  var out_colors = new Array(4)
+  for(var i=0; i<4; ++i) {
+    out_colors[i] = ndarray(out, array.shape, out_stride, i);
   }
-  doColoring(out_colors[0], out_colors[1], out_colors[2], array, cmap, lo, hi)
+  doColoring(out_colors[0], out_colors[1], out_colors[2], out_colors[3], array, cmap, lo, hi, alpha)
   return ndarray(out, out_shape)
 }
